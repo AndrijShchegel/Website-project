70641d39888fe4b52924d617a01c60c70aecda59
AndrijShchegel <andrii.shchegel@gmail.com>
2023-12-26 00:26:45 +0200

    used eslint for better readability

--- public/js/index.js
+++ public/js/index.js
@@ -1,310 +1,312 @@
-"use strict";
 
-import { validateRegistration, validateLogin } from './validation.js';
+import { validateRegistration, validateLogin } from "./validation.js";
 
-const createAuthButtons = (names) => {
-    let popup = document.createElement("div");
-    popup.setAttribute("id", "auth-buttons");
-    document.querySelector("header").appendChild(popup);
 
-    for (let name of names) {
-        let button = document.createElement("button");
-        popup.appendChild(button);
-        button.setAttribute("id", `${name}-button`);
-
-        button.textContent = name.charAt(0).toUpperCase() + name.slice(1);;
-
-        switch (name) {
-            case "login": 
-                button.addEventListener("click", () => createLoginPopup());
-                break;
-            case "register": 
-                button.addEventListener("click", () => createRegisterPopup());
-                break;
-            case "logout": 
-                button.addEventListener("click", () => logout());
-                break;
-            default:
-                console.error("Invalid button name");
-                break;
-        }
-    }
+const closePopup = closeEvent => {
+  document.getElementById("popup-container").remove();
+  document.removeEventListener("click", closeEvent);
 };
 
-const checkForToken = async () => {
-    const storedToken = localStorage.getItem('token');
-    if (storedToken) {
-        try {
-            const response = await fetch('/verify', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ token: storedToken }),
-            });
-            const result = await response.json();
-
-            if (response.ok) {
-                createAuthButtons(["logout"]);
-            } else {
-                createNotification("alert", result.error);
-                createAuthButtons(["login", "register"]);
-            }
-        } catch (error) {
-            createNotification("alert", error);
-            createAuthButtons(["login", "register"]);
-        }
-    } else {
-        createAuthButtons(["login", "register"]);
-    }
-};
-
-checkForToken();
-
-const createRegisterPopup = () => {
-    const closeEvent = closeAfterClickingOutside("register-popup");
-
-    let cotainer = document.createElement("div");
-    cotainer.setAttribute("id", "popup-container");
-    document.body.appendChild(cotainer);
-
-    let register = document.createElement("div");
-    register.setAttribute("id", "register-popup");
-    cotainer.appendChild(register);
-
-    let exitButton = addElement("button", "button", "closebutton");
-    exitButton.textContent = "X";
-    exitButton.addEventListener("click", () => closePopup(closeEvent));
-    register.appendChild(exitButton);
-
-    let name = document.createElement("h2");
-    name.textContent = "BRW";
-    register.appendChild(name);
-    
-    let content = document.createElement("div");
-    content.setAttribute("class", "content");
-    register.appendChild(content);
-
-    let username = addElement("input", "text", "username");
-    username.setAttribute("placeholder", "Username");
-    content.appendChild(username);
-
-    let email = addElement("input", "text", "email");
-    email.setAttribute("placeholder", "Email address");
-    content.appendChild(email);
-
-    let password = addElement("input", "password");
-    password.setAttribute("placeholder", "Password");
-    content.appendChild(password);
-    
-    let passconf = addElement("input", "password", "passconf");
-    passconf.setAttribute("placeholder", "Confirm password");
-    content.appendChild(passconf);
-
-    let confirmButton = addElement("button", "button", "confirmbutton");
-    confirmButton.textContent = "Register";
-    confirmButton.addEventListener("click", () => registrationHandler(content, closeEvent));
-    register.appendChild(confirmButton);
-};
-
-const createLoginPopup = () => {
-    const closeEvent = closeAfterClickingOutside("login-popup");
-
-    let cotainer = document.createElement("div");
-    cotainer.setAttribute("id", "popup-container");
-    document.body.appendChild(cotainer);
-
-    let login = document.createElement("div");
-    login.setAttribute("id", "login-popup");
-    cotainer.appendChild(login);
-
-    let exitButton = addElement("button", "button", "closebutton");
-    exitButton.textContent = "X";
-    exitButton.addEventListener("click", () => closePopup(closeEvent));
-    login.appendChild(exitButton);
-
-    let name = document.createElement("h2");
-    name.textContent = "BRW";
-    login.appendChild(name);
-    
-    let content = document.createElement("div");
-    content.setAttribute("class", "content");
-    login.appendChild(content);
-
-    let email = addElement("input", "text", "email");
-    email.setAttribute("placeholder", "Email address");
-    content.appendChild(email);
-
-    let password = addElement("input", "password");
-    password.setAttribute("placeholder", "Password");
-    content.appendChild(password);
-    
-    let confirmButton = addElement("button", "button", "confirmbutton");
-    confirmButton.textContent = "Login";
-    confirmButton.addEventListener("click", () => loginHandler(content, closeEvent));
-    login.appendChild(confirmButton);
-}
-
-const logout = () => {
-    document.querySelector("#auth-buttons").remove();
-    localStorage.removeItem("token");
-    createAuthButtons(["login", "register"]);
-}
-
-const closeAfterClickingOutside = (id) => {
-    let isFirstClick  = true;
-    const eventListener = (ev) => {
-        if (isFirstClick) {
-            isFirstClick = false;
-            return;
-        }
-
-        const element = document.getElementById(`${id}`);
-        if (element && !element.contains(ev.target)) {
-            closePopup(eventListener)
-        }
-    };
-
-    document.addEventListener("click", eventListener);
-
-    return eventListener;
-};
-
-const registrationHandler = (content, closeEvent) => {
-    let username = content.querySelector("#username").value.trim();
-    let email = content.querySelector("#email").value.trim();
-    let password = content.querySelector("#password").value;
-    let passconf = content.querySelector("#passconf").value;
-
-    removeErrorContainer(content);
-    let errorMessages = validateRegistration(username, email, password, passconf);
-
-    if (errorMessages.length === 0) {
-        submitRegistration(username, email, password, content, closeEvent);
-    } else {
-        displayErrors(errorMessages, content);
-    }
-}
-
-const removeErrorContainer = (content) => {
-    let errorContainer = content.querySelector("#errorContainer");
-    if (errorContainer) {
-        errorContainer.remove();
-    }
+const addElement = (element, type, name = type) => {
+  const elem = document.createElement(`${element}`);
+  elem.setAttribute("type", `${type}`);
+  elem.setAttribute("id", `${name}`);
+  return elem;
 };
 
 const displayErrors = (messages, content) => {
-    let errorDiv = document.createElement("div");
-    errorDiv.setAttribute("id", "errorContainer");
+  const errorDiv = document.createElement("div");
+  errorDiv.setAttribute("id", "errorContainer");
 
-    messages.forEach((message) => {
-        let errorMessage = document.createElement("p");
-        errorMessage.textContent = message;
-        errorDiv.appendChild(errorMessage);
-    });
+  messages.forEach(message => {
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent = message;
+    errorDiv.appendChild(errorMessage);
+  });
 
-    content.appendChild(errorDiv);
-}
+  content.appendChild(errorDiv);
+};
 
-const submitRegistration = async (username, email, password, content, closeEvent) => {
-    try {
-        const response = await fetch('/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username,
-                email,
-                password,
-            }),
-        });
-        
-        const data = await response.json();
-
-        if (response.ok) {
-            createNotification("alert success", data.message);
-            closePopup(closeEvent);
-        } else {
-            displayErrors([data.error], content);
-        }
-    } catch (error) {
-        displayErrors(["Error connecting to server:", error.message], content);
-    }
-}
-
-const loginHandler = (content, closeEvent) => {
-    let email = content.querySelector("#email").value.trim();
-    let password = content.querySelector("#password").value;
-
-    removeErrorContainer(content);
-    let errors = validateLogin(email, password);
-
-    if (errors.length === 0) {
-        submitLogin(email, password, content, closeEvent);
-    } else {
-        displayErrors(errors, content);
-    }
-}
-
-
-const submitLogin = async (email, password, content, closeEvent) => {
-    try {
-        const response = await fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email,
-                password,
-            }),
-        });
-        
-        const data = await response.json();
-
-        if (response.ok) {
-            localStorage.setItem('token', data.token);
-            createNotification("alert success", data.message);
-            closePopup(closeEvent);
-            document.querySelector("#auth-buttons").remove();
-            checkForToken();
-        } else {
-            displayErrors([data.error], content);
-        }
-    } catch (error) {
-        displayErrors(["Error connecting to server:", error.message], content);
-    }
-}
-
-const closePopup = (closeEvent) => {
-    document.getElementById("popup-container").remove();
-    document.removeEventListener("click", closeEvent);
-}
-
-const addElement = (element, type, name = type) => {
-    let elem = document.createElement(`${element}`);
-    elem.setAttribute("type", `${type}`);
-    elem.setAttribute("id", `${name}`);
-    return elem;
+const removeErrorContainer = content => {
+  const errorContainer = content.querySelector("#errorContainer");
+  if (errorContainer) {
+    errorContainer.remove();
+  }
 };
 
 const createNotification = (className, text) => {
-    let notification = document.getElementById("notification");
+  let notification = document.getElementById("notification");
 
-    if (!notification) {
-        notification = document.createElement("div");
-        notification.setAttribute("id", "notification");
-        document.body.appendChild(notification);
+  if (!notification) {
+    notification = document.createElement("div");
+    notification.setAttribute("id", "notification");
+    document.body.appendChild(notification);
+  }
+  const alert = document.createElement("div");
+  alert.setAttribute("class", `${className}`);
+  alert.textContent = text;
+  alert.addEventListener("click", () => {
+    alert.remove();
+    if (notification.childElementCount === 0) {
+      notification.remove();
     }
-    let alert = document.createElement("div");
-    alert.setAttribute("class", `${className}`);
-    alert.textContent = text;
-    alert.addEventListener("click", () => {
-        alert.remove();
-        if (notification.childElementCount === 0) {
-            notification.remove();
-        }
+  });
+  notification.appendChild(alert);
+};
+
+const closeAfterClickingOutside = id => {
+  let isFirstClick  = true;
+  const eventListener = ev => {
+    if (isFirstClick) {
+      isFirstClick = false;
+      return;
+    }
+
+    const element = document.getElementById(`${id}`);
+    if (element && !element.contains(ev.target)) {
+      closePopup(eventListener);
+    }
+  };
+
+  document.addEventListener("click", eventListener);
+
+  return eventListener;
+};
+
+const submitRegistration = async (username, email, password, content, closeEvent) => {
+  try {
+    const response = await fetch("/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
     });
-    notification.appendChild(alert);
-}
+
+    const data = await response.json();
+
+    if (response.ok) {
+      createNotification("alert success", data.message);
+      closePopup(closeEvent);
+    } else {
+      displayErrors([data.error], content);
+    }
+  } catch (error) {
+    displayErrors(["Error connecting to server:", error.message], content);
+  }
+};
+
+const submitLogin = async (email, password, content, closeEvent) => {
+  try {
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      localStorage.setItem("token", data.token);
+
+      createNotification("alert success", data.message);
+
+      closePopup(closeEvent);
+
+      document.querySelector("#auth-buttons").remove();
+      createAuthButtons(["logout"]);
+    } else {
+      displayErrors([data.error], content);
+    }
+  } catch (error) {
+    displayErrors(["Error connecting to server:", error.message], content);
+  }
+};
+
+const registrationHandler = (content, closeEvent) => {
+  const username = content.querySelector("#username").value.trim();
+  const email = content.querySelector("#email").value.trim();
+  const password = content.querySelector("#password").value;
+  const passconf = content.querySelector("#passconf").value;
+
+  removeErrorContainer(content);
+  const errorMessages = validateRegistration(username, email, password, passconf);
+
+  if (errorMessages.length === 0) {
+    submitRegistration(username, email, password, content, closeEvent);
+  } else {
+    displayErrors(errorMessages, content);
+  }
+};
+
+const loginHandler = (content, closeEvent) => {
+  const email = content.querySelector("#email").value.trim();
+  const password = content.querySelector("#password").value;
+
+  removeErrorContainer(content);
+  const errors = validateLogin(email, password);
+
+  if (errors.length === 0) {
+    submitLogin(email, password, content, closeEvent);
+  } else {
+    displayErrors(errors, content);
+  }
+};
+
+const createRegisterPopup = () => {
+  const closeEvent = closeAfterClickingOutside("register-popup");
+
+  const cotainer = document.createElement("div");
+  cotainer.setAttribute("id", "popup-container");
+  document.body.appendChild(cotainer);
+
+  const register = document.createElement("div");
+  register.setAttribute("id", "register-popup");
+  cotainer.appendChild(register);
+
+  const exitButton = addElement("button", "button", "closebutton");
+  exitButton.textContent = "X";
+  exitButton.addEventListener("click", () => closePopup(closeEvent));
+  register.appendChild(exitButton);
+
+  const name = document.createElement("h2");
+  name.textContent = "BRW";
+  register.appendChild(name);
+
+  const content = document.createElement("div");
+  content.setAttribute("class", "content");
+  register.appendChild(content);
+
+  const username = addElement("input", "text", "username");
+  username.setAttribute("placeholder", "Username");
+  content.appendChild(username);
+
+  const email = addElement("input", "text", "email");
+  email.setAttribute("placeholder", "Email address");
+  content.appendChild(email);
+
+  const password = addElement("input", "password");
+  password.setAttribute("placeholder", "Password");
+  content.appendChild(password);
+
+  const passconf = addElement("input", "password", "passconf");
+  passconf.setAttribute("placeholder", "Confirm password");
+  content.appendChild(passconf);
+
+  const confirmButton = addElement("button", "button", "confirmbutton");
+  confirmButton.textContent = "Register";
+  confirmButton.addEventListener("click", () => registrationHandler(content, closeEvent));
+  register.appendChild(confirmButton);
+};
+
+const createLoginPopup = () => {
+  const closeEvent = closeAfterClickingOutside("login-popup");
+
+  const cotainer = document.createElement("div");
+  cotainer.setAttribute("id", "popup-container");
+  document.body.appendChild(cotainer);
+
+  const login = document.createElement("div");
+  login.setAttribute("id", "login-popup");
+  cotainer.appendChild(login);
+
+  const exitButton = addElement("button", "button", "closebutton");
+  exitButton.textContent = "X";
+  exitButton.addEventListener("click", () => closePopup(closeEvent));
+  login.appendChild(exitButton);
+
+  const name = document.createElement("h2");
+  name.textContent = "BRW";
+  login.appendChild(name);
+
+  const content = document.createElement("div");
+  content.setAttribute("class", "content");
+  login.appendChild(content);
+
+  const email = addElement("input", "text", "email");
+  email.setAttribute("placeholder", "Email address");
+  content.appendChild(email);
+
+  const password = addElement("input", "password");
+  password.setAttribute("placeholder", "Password");
+  content.appendChild(password);
+
+  const confirmButton = addElement("button", "button", "confirmbutton");
+  confirmButton.textContent = "Login";
+  confirmButton.addEventListener("click", () => loginHandler(content, closeEvent));
+  login.appendChild(confirmButton);
+};
+
+const logout = () => {
+  document.querySelector("#auth-buttons").remove();
+  localStorage.removeItem("token");
+  createAuthButtons(["login", "register"]);
+};
+
+const createAuthButtons = names => {
+  const popup = document.createElement("div");
+  popup.setAttribute("id", "auth-buttons");
+  document.querySelector("header").appendChild(popup);
+
+  for (const name of names) {
+    const button = document.createElement("button");
+    popup.appendChild(button);
+    button.setAttribute("id", `${name}-button`);
+
+    button.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+
+    switch (name) {
+    case "login":
+      button.addEventListener("click", () => createLoginPopup());
+      break;
+    case "register":
+      button.addEventListener("click", () => createRegisterPopup());
+      break;
+    case "logout":
+      button.addEventListener("click", () => logout());
+      break;
+    default:
+      console.error("Invalid button name");
+      break;
+    }
+  }
+};
+
+const checkForToken = async () => {
+  const storedToken = localStorage.getItem("token");
+  if (storedToken) {
+    try {
+      const response = await fetch("/verify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: storedToken }),
+      });
+      const result = await response.json();
+
+      if (response.ok) {
+        createAuthButtons(["logout"]);
+      } else {
+        createNotification("alert", result.error);
+        createAuthButtons(["login", "register"]);
+      }
+    } catch (error) {
+      createNotification("alert", error);
+      createAuthButtons(["login", "register"]);
+    }
+  } else {
+    createAuthButtons(["login", "register"]);
+  }
+};
+
+checkForToken();
--- server.js
+++ server.js
@@ -1,79 +1,78 @@
-"use strict";
-const express = require('express');
-const { Pool } = require('pg');
-const path = require('path');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const { Pool } = require("pg");
+const path = require("path");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = 3000;
 
 const pool = new Pool({
-    user: 'andrij',
-    host: 'localhost',
-    database: 'Web-database',
-    password: 'passwd',
-    port: 5432,
+  user: "andrij",
+  host: "localhost",
+  database: "Web-database",
+  password: "passwd",
+  port: 5432,
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html');
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
 });
 
-app.post('/register', async (req, res) => {
-    const { username, email, password } = req.body;
-    const client = await pool.connect();
-    const select = `SELECT * FROM users WHERE email = $1`
-    const insert = 'INSERT INTO users (name, email, password) VALUES ($1, $2, $3)';
-    try {
-        let results = await client.query(select, [email]);
-        if(results.rows.length > 0) {
-            res.status(400).json({ error: "Email already registered"});
-        } else {
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const client = await pool.connect();
+  const select = "SELECT * FROM users WHERE email = $1";
+  const insert = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)";
+  try {
+    const results = await client.query(select, [email]);
+    if (results.rows.length > 0) {
+      res.status(400).json({ error: "Email already registered" });
+    } else {
 
-        await client.query(insert, [username, email, password]);
-        
-        res.status(200).json({ message: 'Registration successful!' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    } finally {
-        client.release();
+      await client.query(insert, [username, email, password]);
+
+      res.status(200).json({ message: "Registration successful!" });
     }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    client.release();
+  }
 });
 
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    const client = await pool.connect();
-    const select = `SELECT * FROM users WHERE email = $1 AND password = $2`
-    try {
-        let results = await client.query(select, [email, password]);
-        if(results.rows.length === 0) {
-            res.status(400).json({ error: 'Invalid email or password'});
-        } else {
-            let token = jwt.sign({ email: email }, 'secret-1998', { expiresIn: '1d' });
-            res.status(200).json({ message: 'Login successful!', token });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    } finally {
-        client.release();
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const client = await pool.connect();
+  const select = "SELECT * FROM users WHERE email = $1 AND password = $2";
+  try {
+    const results = await client.query(select, [email, password]);
+    if (results.rows.length === 0) {
+      res.status(400).json({ error: "Invalid email or password" });
+    } else {
+      const token = jwt.sign({ email }, "secret-1998", { expiresIn: "1d" });
+      res.status(200).json({ message: "Login successful!", token });
     }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    client.release();
+  }
 });
 
-app.post('/verify', (req, res) => {
-    const { token } = req.body;
-    jwt.verify(token, 'secret-1998', (err, decoded) => {
-        if (err) {
-            res.status(401).json({ error: 'Your token had expired. Please re-login.' });
-        } else {
-            res.status(200).json({ email: decoded.email });
-        }
-    });
+app.post("/verify", (req, res) => {
+  const { token } = req.body;
+  jwt.verify(token, "secret-1998", (err, decoded) => {
+    if (err) {
+      res.status(401).json({ error: "Your token had expired. Please re-login." });
+    } else {
+      res.status(200).json({ email: decoded.email });
+    }
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
